c6e92bc459737a6a660ea1e0e024bc86d36f20b6
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-05-17 16:56:01 +1200

    'nextJS storybook'

---
+++ src/components/BackHome/BackHome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const BackHome = () => {
+  return (
+    <h2 className="text-center">
+      <Link href="/">Back to home</Link>
+    </h2>
+  );
+};
+
+export default BackHome;
---
+++ src/pages/posts/first-post.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import BackHome from "@/components/BackHome/BackHome";
+import Head from "next/head";
+
+const FirstPost = () => {
+  return (
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1 className="text-2xl font-bold text-center"> First Post </h1>
+      <BackHome />
+    </div>
+  );
+};
+
+export default FirstPost;
