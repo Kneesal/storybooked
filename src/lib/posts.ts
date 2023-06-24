a4788535ca46ff41d45bd4cbdb8ef1c050facb23
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-06-25 08:53:55 +1200

    add getstaticprops

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,10 +1,17 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { title } from "process";
+
+interface AllPosts {
+  id: string;
+  title: string;
+  date: string;
+}
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
-export const getSortedPostsData = () => {
+export const getSortedPostsData = (): AllPosts[] => {
   const fileNames = fs.readdirSync(postsDirectory); // Get file names under /posts
 
   const allPostsData = fileNames.map((fileName) => {
@@ -14,5 +21,17 @@ export const getSortedPostsData = () => {
     const fileContents = fs.readFileSync(fullPath); //read contents of file
 
     const matterResult = matter(fileContents); // Use gray-matter to parse the post metadata section
+
+    return {
+      id,
+      ...matterResult.data,
+    } as AllPosts;
+  });
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
   });
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import Head from "next/head";
 import { siteTitle, Layout } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import postcss from "postcss";
+import { InferGetStaticPropsType } from "next";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({
+  allPostsData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +24,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>passionate about software engineering</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
