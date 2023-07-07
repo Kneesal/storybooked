2976e293a5f1c276dad66936e9cbfda0b57c43d7
Nisal Cottingham <nisalcottingham@Nisals-MacBook-Pro.local>
2023-07-08 11:21:20 +1200

    get static paths and polish everyting up

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,10 +1,14 @@
 import { Layout } from "@/components/Layout";
 import { getAllPostsId, getPostData } from "@/lib/posts";
+import Head from "next/head";
+import Date from "../../components/Date/Date";
+import utilStyles from "../../styles/utils.module.css";
 
 interface PostProps {
   postData: {
     title: string;
     id: string;
+    contentHtml: string;
     date: string;
   };
 }
@@ -12,17 +16,24 @@ interface PostProps {
 export default function Post({ postData }: PostProps) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div style={{ marginTop: "30px" }}>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </div>
+      </article>
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostsId();
+  const paths = await getAllPostsId();
   // Return a list of possible value for id
   return {
     paths,
@@ -32,7 +43,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: any) {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
